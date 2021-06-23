9135e1da64b67e9181b5efc1ba1809559fb8b8b7
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-06-23 17:46:08 -0300

    CRUD para professores

--- data/Professor.php
+++ data/Professor.php
@@ -1,9 +1,66 @@
 <?php
-class Professor extends data{
+class Professores extends data{
 
+    public function Get($Id){
+        $sql = "SELECT * FROM professores WHERE Id = :Id";
+        $sql = $db->prepare($sql);
+        $sql->bindValue(':Id', $Id);
+        $sql->execute();
 
+        if($sql->rowCount() > 0){
+            $sql = $sql->fetch(PDO::FETCH_ASSOC);
+            return $sql;
+        }
+        return false;
+    }
+    public function GetAll(){
+        $sql = "SELECT * FROM professores ORDER BY Nome ASC";
+        $sql = $db->prepare($sql);
+        $sql->bindValue(':Id', $Id);
+        $sql->execute();
 
+        if($sql->rowCount() > 0){
+            $sql = $sql->fetch(PDO::FETCH_ASSOC);
+            return $sql;
+        }
+        return false;
+    }
+    public function Insert($professor){
+        $sql = "INSERT INTO professores (Id, Nome, CPF, data_nasc) VALUES (:Id, :Nome, :CPF, :data_nasc)";
+        $sql = $this->db->prepare($sql);
+        $sql->bindValue(':Id', $professor->Id);
+        $sql->bindValue(':Nome', $professor->Nome);
+        $sql->bindValue(':CPF', $professor->CPF);
+        $sql->bindValue(':data_nasc', $professor->Data_Nasc);
+        if($sql->execute()){
+            return true;
+        }
+        return false;
+    }
+    public function Update($professor){
+        $sql = "UPDATE professores SET Nome = :Nome, CPF = :CPF, data_nasc = :data_nasc WHERE Id = :Id";
+        $sql = $this->db->prepare($sql);
+        $sql->bindValue(':Id', $professor->Id);
+        $sql->bindValue(':Nome', $professor->Nome);
+        $sql->bindValue(':CPF', $professor->CPF);
+        $sql->bindValue(':data_nasc', $professor->Data_Nasc);
+        if($sql->execute()){
+            return true;
+        }
+        return false;
+    }
 
+    public function Delete($Id){
+        $sql = "DELETE FROM professores WHERE Id = :Id";
+        $sql = $db->prepare($sql);
+        $sql->bindValue(':Id', $Id);
+        $sql->execute();
+
+        if($sql->execute()){
+            return true;
+        }
+        return false;
+    }
 
     
 }
